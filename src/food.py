39559c09ab06a91f0d43978fe8c19f6f4fadc7e5
dmccoystephenson <dmccoystephenson@gmail.com>
2022-08-09 22:50:57 -0600

    Made the player depend on food entities for energy.

--- src/food.py
+++ src/food.py
@@ -8,6 +8,10 @@ class Food(Entity):
     def __init__(self):
         Entity.__init__(self, "Food")
         self.color = (random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255))
+        self.energy = random.randrange(1, 10)
     
     def getColor(self):
         return self.color
+    
+    def getEnergy(self):
+        return self.energy
