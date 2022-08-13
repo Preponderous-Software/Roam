1983a9765d1829b32b9acc72090f8f9de4547081
dmccoystephenson <dmccoystephenson@gmail.com>
2022-08-12 19:12:00 -0600

    Added the Grass entity.

---
+++ src/grass.py
@@ -0,0 +1,13 @@
+import random
+from entity import Entity
+
+
+# @author Daniel McCoy Stephenson
+# @since August 8th, 2022
+class Grass(Entity):
+    def __init__(self):
+        Entity.__init__(self, "Grass")
+        self.color = ((0, random.randrange(150, 200), 0))
+    
+    def getColor(self):
+        return self.color
