0dcba9ce699d7ce8151ee12bab9daffab0ec1945
dmccoystephenson <dmccoystephenson@gmail.com>
2022-08-16 23:19:58 -0600

    Replaced the energy display with an energy bar at the bottom of the screen.

---
+++ src/energyBar.py
@@ -0,0 +1,18 @@
+from graphik import Graphik
+from player import Player
+
+# @author Daniel McCoy Stephenson
+# @since August 16th, 2022
+class EnergyBar:
+    def __init__(self, graphik: Graphik, player: Player):
+        self.graphik = graphik
+        self.player = player
+    
+    def draw(self):
+        x, y = self.graphik.getGameDisplay().get_size()
+        xpos = 0
+        ypos = y - y/64
+        width = x * (self.player.getEnergy()/self.player.getMaxEnergy())
+        height = y/64
+        color = (255, 215, 73)
+        self.graphik.drawRectangle(xpos, ypos, width, height, color)
