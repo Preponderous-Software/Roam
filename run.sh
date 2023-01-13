ff2b521906b1da0d4123ec16db12264068695fef
dmccoystephenson <dmccoystephenson@gmail.com>
2023-01-12 18:32:23 -0700

    Added echo statements to run.sh script.

--- run.sh
+++ run.sh
@@ -1,9 +1,15 @@
 # get the latest version of the code
+echo "Pulling latest version of code from GitHub"
 git pull
 
+# print branch status
+echo "Current branch: $(git branch --show-current)"
+
 # check that dependencies are installed
+echo "Checking dependencies"
 pip install pygame --pre
 pip install -r requirements.txt
 
 # start program
+echo "Starting program"
 python src/roam.py
