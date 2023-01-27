fa9a6340f474193972e70a952b94cce806c8a9bb
dmccoystephenson <dmccoystephenson@gmail.com>
2023-01-26 21:59:26 -0700

    Made the tick persistent.

--- src/world/tickCounter.py
+++ src/world/tickCounter.py
@@ -1,3 +1,8 @@
+import json
+
+import jsonschema
+
+
 class TickCounter:
     def __init__(self):
         self.tick = 0
@@ -7,3 +12,24 @@ class TickCounter:
     
     def incrementTick(self):
         self.tick += 1
+    
+    def save(self):
+        jsonTick = {}
+        jsonTick["tick"] = self.getTick()
+        
+        # validate
+        tickSchema = json.load(open("schemas/tick.json"))
+        jsonschema.validate(jsonTick, tickSchema)
+        
+        path = "data/tick.json"
+        json.dump(jsonTick, open(path, "w"), indent=4)
+    
+    def load(self):
+        path = "data/tick.json"
+        jsonTick = json.load(open(path))
+        
+        # validate
+        tickSchema = json.load(open("schemas/tick.json"))
+        jsonschema.validate(jsonTick, tickSchema)
+        
+        self.tick = int(jsonTick["tick"])
