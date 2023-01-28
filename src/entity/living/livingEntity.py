f211a4b0e22b1d44e21bd684270fabf05810aa25
dmccoystephenson <dmccoystephenson@gmail.com>
2023-01-28 14:09:26 -0700

    Modified needsEnergy() method in the LivingEntity class.

--- src/entity/living/livingEntity.py
+++ src/entity/living/livingEntity.py
@@ -36,7 +36,7 @@ class LivingEntity(DrawableEntity):
             self.energy -= amount
 
     def needsEnergy(self):
-        return self.energy < self.targetEnergy * 0.95
+        return self.energy < self.targetEnergy * 0.75
     
     def getTargetEnergy(self):
         return self.targetEnergy
