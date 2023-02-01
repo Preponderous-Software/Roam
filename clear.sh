1a0d2f25cf931f8c2e466c3b6493065ee0ec56c3
dmccoystephenson <dmccoystephenson@gmail.com>
2023-01-31 23:03:47 -0700

    Modified clear.sh script to account for roompngs.

--- clear.sh
+++ clear.sh
@@ -1,3 +1,10 @@
 # clear .json files in data directory
 rm -f data/*.json
 rm -f data/rooms/*.json
+
+# clear png files in roompngs directory
+rm -f roompngs/*.png
+
+# rename existing combined.png
+date=$(date +%Y%m%d%H%M%S)
+mv combined.png combined_$date.png
