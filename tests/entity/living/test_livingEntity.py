47333c3b45022edf49fa96db52fca3fb643d3ae3
dmccoystephenson <dmccoystephenson@gmail.com>
2023-02-08 21:44:08 -0700

    Cleaned up test_livingEntity.py a bit.

--- tests/entity/living/test_livingEntity.py
+++ tests/entity/living/test_livingEntity.py
@@ -1,8 +1,11 @@
 import pytest
 from src.entity.living.livingEntity import LivingEntity
 
+def createLivingEntity():
+    return LivingEntity("test", "myimagepath.png", 50, [], 0)
+
 def test_initialization():
-    livingEntity = LivingEntity("test", "myimagepath.png", 50, [], 0)
+    livingEntity = createLivingEntity()
     
     assert(livingEntity.name == "test")
     assert(livingEntity.getEnergy() == 50)
@@ -10,47 +13,47 @@ def test_initialization():
     assert(livingEntity.getTickCreated() == 0)
 
 def test_set_energy():
-    livingEntity = LivingEntity("test", "myimagepath.png", 50, [], 0)
+    livingEntity = createLivingEntity()
     livingEntity.setEnergy(100)
     
     assert(livingEntity.getEnergy() == 100)
     
 
 def test_set_target_energy():
-    livingEntity = LivingEntity("test", "myimagepath.png", 50, [], 0)
+    livingEntity = createLivingEntity()
     livingEntity.setTargetEnergy(100)
     
     assert(livingEntity.getTargetEnergy() == 100)
 
 def test_add_energy():
-    livingEntity = LivingEntity("test", "myimagepath.png", 50, [], 0)
+    livingEntity = createLivingEntity()
     livingEntity.addEnergy(50)
     
     assert(livingEntity.getEnergy() == 100)
 
 def test_remove_energy():
-    livingEntity = LivingEntity("test", "myimagepath.png", 50, [], 0)
+    livingEntity = createLivingEntity()
     livingEntity.removeEnergy(50)
     
     assert(livingEntity.getEnergy() == 0)
 
 def test_needs_energy():
-    livingEntity = LivingEntity("test", "myimagepath.png", 50, [], 0)
+    livingEntity = createLivingEntity()
     assert(livingEntity.needsEnergy() == False)
     
     livingEntity.setEnergy(livingEntity.getTargetEnergy() / 2 - 1)
     assert(livingEntity.needsEnergy() == True)
 
 def test_get_age():
-    livingEntity = LivingEntity("test", "myimagepath.png", 50, [], 0)
+    livingEntity = createLivingEntity()
     assert(livingEntity.getAge(100) == 100)
 
 def test_kill():
-    livingEntity = LivingEntity("test", "myimagepath.png", 50, [], 0)
+    livingEntity = createLivingEntity()
     livingEntity.kill()
     
     assert(livingEntity.getEnergy() == 0)
 
 def test_can_eat():
-    livingEntity = LivingEntity("test", "myimagepath.png", 50, [], 0)
+    livingEntity = createLivingEntity()
     assert(livingEntity.canEat("test") == False)
