8938e5639004c7ce52fc5339470f35044d2c1080
dmccoystephenson <dmccoystephenson@gmail.com>
2023-02-10 20:55:43 -0700

    Added python and pip checks to run.sh script.

--- run.sh
+++ run.sh
@@ -10,6 +10,16 @@ echo "Current version: $(cat version.txt)"
 
 # check that dependencies are installed
 echo "Checking dependencies"
+if ! command -v python &> /dev/null
+then
+    echo "Python could not be found. Download it from https://www.python.org/downloads/"
+    exit
+fi
+if ! command -v pip &> /dev/null
+then
+    echo "Pip could not be found. Download it from https://pip.pypa.io/en/stable/installation/ or run 'python -m ensurepip' in a terminal"
+    exit
+fi
 pip install pygame --pre --quiet
 pip install -r requirements.txt --quiet
 
