e4f0320978f87442c22e3f1a48f6b78a6a362ce4
dmccoystephenson <dmccoystephenson@gmail.com>
2023-02-10 20:48:24 -0700

    Made test.sh generate coverage file and display coverage in console.

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 # /bin/bash
 # Usage: ./test.sh
 
-pytest --verbose -vv
+# generate coverage file named "cov.xml"
+python -m pytest --verbose -vv --cov=src --cov-report=term-missing --cov-report=xml:cov.xml
